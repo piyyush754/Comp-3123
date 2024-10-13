97ecfc788e481623d30bc959da2ca37faa2e7fbd
Piyushbhai Patel <101410303@georgebrown.ca>
2024-10-13 15:09:02 -0400

    GG

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(8082, () => {
+    console.log(`Server is listening on port 8082`);
 });
